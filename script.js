17c34b8eda3c64496a228bac7540ed768d1f8d81
Ryan Permana <bismillahproduktif6@gmail.com>
2024-03-24 09:11:21 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// feather icon
+
+feather.replace();
+
+
+
+// toggle class active 
+
+const navbarNav = document.querySelector ('.navbar-nav');
+
+// if hamburger menu click 
+
+document.querySelector('#menu').onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    event.preventDefault();
+};
+
+alert ('website ini belum berfungsi sepenuhnya dan akan mengalami pembaruan seiring berjalannya waktu');
